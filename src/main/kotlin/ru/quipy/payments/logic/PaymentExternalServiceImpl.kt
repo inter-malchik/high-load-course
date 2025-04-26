42baaa42139f41346479ef5d9dd25050ff23a878
Nikita Kornilov <kornik36@gmail.com>
2025-04-26 14:23:07 +0300

    case 7: refactor

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -6,7 +6,7 @@ import io.github.resilience4j.retry.Retry
 import io.github.resilience4j.retry.RetryConfig
 import org.eclipse.jetty.http.HttpStatus
 import org.slf4j.LoggerFactory
-import ru.quipy.common.utils.LeakingBucketRateLimiter
+import ru.quipy.common.utils.TokenBucketRateLimiter
 import ru.quipy.core.EventSourcingService
 import ru.quipy.payments.api.PaymentAggregate
 import java.io.InterruptedIOException
@@ -16,11 +16,11 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 import java.time.Duration.ofMillis
-import java.time.Duration.ofSeconds
-import java.util.*
+import java.util.UUID
 import java.util.concurrent.CompletionStage
 import java.util.concurrent.Executors
 import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
 import java.util.function.Supplier
 
 
@@ -46,17 +46,28 @@ class PaymentExternalSystemAdapterImpl(
     private val httpClient = HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_2)
         .build()
-    private val rateLimiter = LeakingBucketRateLimiter(rateLimitPerSec.toLong(), ofSeconds(1), rateLimitPerSec)
-    private val semaphore = Semaphore(parallelRequests)
-    private val retryTimeout = ofSeconds(1)
-    private val retryExecutor = Executors.newSingleThreadScheduledExecutor()
 
-    override fun performPaymentAsync(paymentId: UUID, amount: Int, paymentStartedAt: Long, deadline: Long) {
+    private val rateLimiter = TokenBucketRateLimiter(
+        rate = rateLimitPerSec,
+        window = 1005,
+        bucketMaxCapacity = rateLimitPerSec,
+        timeUnit = TimeUnit.MILLISECONDS
+    )
+
+    private val semaphore = Semaphore(parallelRequests)
+    private val retryTimeout = ofMillis(100)
+    private val retryExecutor = Executors.newScheduledThreadPool(8)
+
+    override fun performPaymentAsync(
+        paymentId: UUID,
+        amount: Int,
+        paymentStartedAt: Long,
+        deadline: Long
+    ) {
         semaphore.acquire()
         logger.warn("[$accountName] Submitting payment request for payment $paymentId")
         val transactionId = UUID.randomUUID()
         try {
-            rateLimiter.tickBlocking()
             logger.info("[$accountName] Submit for $paymentId , txId: $transactionId")
 
             // Фиксируем факт отправки платежа независимо от результата.
@@ -64,37 +75,14 @@ class PaymentExternalSystemAdapterImpl(
                 it.logSubmission(success = true, transactionId, now(), ofMillis(now() - paymentStartedAt))
             }
 
-            val localRetryConfig = RetryConfig.custom<ExternalSysResponse>()
-                .maxAttempts(5)
-                .waitDuration(retryTimeout)
-                .retryOnResult { response: ExternalSysResponse ->
-                    now() <= deadline && (!response.result || response.code == HttpStatus.TOO_MANY_REQUESTS_429)
-                }
-                .retryOnException { exception ->
-                    (exception is SocketTimeoutException || exception is InterruptedIOException) && (now() <= deadline)
-                }
-                .failAfterMaxAttempts(true)
-                .build()
+            val localRetry = buildRetryConfig(deadline, transactionId)
+            val httpCallSupplier = buildHttpSupplier(transactionId, paymentId, amount)
 
-            val localRetry = Retry.of("payment-${transactionId}", localRetryConfig)
-
-            var httpRequest = HttpRequest
-                .newBuilder()
-                .uri(URI("$paymentUrl?serviceName=${serviceName}&accountName=${accountName}&transactionId=$transactionId&paymentId=$paymentId&amount=$amount"))
-                .POST(HttpRequest.BodyPublishers.noBody())
-                .build()
-
-            val httpCallSupplier = Supplier<CompletionStage<HttpResponse<String>>> {
-                httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
-            }
-
-            val retryable = Retry.decorateCompletionStage(
+            Retry.decorateCompletionStage(
                 localRetry,
                 retryExecutor,
                 httpCallSupplier
             ).get()
-
-            retryable
                 .thenApply { response ->
                     try {
                         val readValue = mapper.readValue(response.body(), ExternalSysResponse::class.java)
@@ -140,6 +128,39 @@ class PaymentExternalSystemAdapterImpl(
         }
     }
 
+    private fun buildRetryConfig(deadline: Long, transactionId: UUID?): Retry {
+        val localRetryConfig = RetryConfig.custom<ExternalSysResponse>()
+            .maxAttempts(5)
+            .waitDuration(retryTimeout)
+            .retryOnResult { response: ExternalSysResponse ->
+                now() < deadline && (!response.result || response.code == HttpStatus.TOO_MANY_REQUESTS_429)
+            }
+            .retryOnException { exception ->
+                (now() < deadline) && (exception is SocketTimeoutException || exception is InterruptedIOException)
+            }
+            .failAfterMaxAttempts(true)
+            .build()
+
+        return Retry.of("payment-${transactionId}", localRetryConfig)
+    }
+
+    private fun buildHttpSupplier(
+        transactionId: UUID?,
+        paymentId: UUID,
+        amount: Int
+    ): Supplier<CompletionStage<HttpResponse<String>>> {
+        val httpRequest = HttpRequest
+            .newBuilder()
+            .uri(URI("$paymentUrl?serviceName=${serviceName}&accountName=${accountName}&transactionId=$transactionId&paymentId=$paymentId&amount=$amount"))
+            .POST(HttpRequest.BodyPublishers.noBody())
+            .build()
+
+        val httpCallSupplier = Supplier<CompletionStage<HttpResponse<String>>> {
+            rateLimiter.tickBlocking()
+            httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString())
+        }
+        return httpCallSupplier
+    }
 
     override fun price() = properties.price
 
